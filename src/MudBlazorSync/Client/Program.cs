c51dc0aeda3db65c1f5881339ba45e1d6ef61b4b
Sergiusz Kriwobokow <writesd@hotmail.com>
2023-04-09 03:41:33 +0200

    upgrade dependencies

--- src/MudBlazorSync/Client/Program.cs
+++ src/MudBlazorSync/Client/Program.cs
@@ -8,6 +8,7 @@ using MudBlazorSync.Application;
 using MudBlazorSync.Domain;
 
 // Version 20.4.0.40 = ODg2NDk1QDMyMzAyZTM0MmUzMGM1UDJiM0N5d0VjaXY4Z1JXTHYxTENtMEh1YnlrRmEyMmpMOFJmTWUzcTQ9
+//Version 21.1.38 = MTY2MDk0NEAzMjMxMmUzMTJlMzMzOEZBVkp3Ui9aM0UwbGlLczJaS1o2aXpNYWRlTzRpYU1ZY0xoOHVQRjUzYjg9
 namespace Sync
 {
     public class Program
@@ -15,7 +16,7 @@ namespace Sync
         public static async Task Main(string[] args)
         {
             // Register Syncfusion license
-            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("ODg2NDk1QDMyMzAyZTM0MmUzMGM1UDJiM0N5d0VjaXY4Z1JXTHYxTENtMEh1YnlrRmEyMmpMOFJmTWUzcTQ9");
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MTY2MDk0NEAzMjMxMmUzMTJlMzMzOEZBVkp3Ui9aM0UwbGlLczJaS1o2aXpNYWRlTzRpYU1ZY0xoOHVQRjUzYjg9");
 
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
